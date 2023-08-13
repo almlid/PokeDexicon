b1b2f8cf774d1fe4ab7757292c43f014d1dc14ac
ariavalenci <aria@valenci.no>
2023-08-13 12:55:38 +0200

    added pagination

--- src/components/SearchSection.tsx
+++ src/components/SearchSection.tsx
@@ -14,6 +14,8 @@ const SearchSection = ({
   const [fetchLimit, setFetchLimit] = useState(20);
   const [offset, setOffset] = useState(0);
   const maxOffset = resultCount - fetchLimit;
+  const pages = Math.ceil(resultCount / fetchLimit);
+  const currentPage = Math.ceil(offset / fetchLimit + 1);
 
   const [currentEntries, setCurrentEntries] = useState<IPokemonPreview[]>();
 
@@ -44,17 +46,112 @@ const SearchSection = ({
     setOffset(newOffset <= 0 ? 0 : newOffset);
   };
 
+  const jumpToPage = (page: number) => {
+    setOffset((page - 1) * fetchLimit);
+  };
+
+  const renderPageNumberButtons = () => {
+    const buttons = [];
+    for (let i = 1; i <= pages; i++) {
+      if (i === currentPage) {
+        buttons.push(
+          <span className="active" key={i}>
+            {i}
+          </span>
+        );
+      } else if (
+        i !== 1 &&
+        i !== pages &&
+        i >= currentPage - 2 &&
+        i <= currentPage + 2
+      ) {
+        buttons.push(
+          <button key={i} onClick={() => jumpToPage(i)}>
+            {i}
+          </button>
+        );
+      }
+    }
+    return buttons;
+  };
+
+  const renderPageNumberSelectOptions = (
+    firstPage: number,
+    lastPage: number
+  ) => {
+    const options = [
+      <option key={0} disabled>
+        ...
+      </option>,
+    ];
+    for (let i = firstPage; i <= lastPage; i++) {
+      options.push(
+        <option key={i} value={i - 1}>
+          {i}
+        </option>
+      );
+    }
+    return options;
+  };
+
   return (
     <SearchSectionWrapper>
-      <p>Results: {resultCount}</p>
-      <button onClick={getPrevious}>Previous {fetchLimit} </button>
+      <p>
+        Results: {resultCount} -- Current results: {offset + 1}-
+        {offset + fetchLimit}
+      </p>
+      <p>
+        Pages: {pages} -- Current page: {currentPage}
+      </p>
       {currentEntries && (
         <PokedexEntries
           entries={currentEntries}
           setCurrentPokemon={setCurrentPokemon}
         ></PokedexEntries>
       )}
-      <button onClick={getNext}>Next {fetchLimit} </button>
+      <PageNavWrapper>
+        <button onClick={getPrevious}>←</button>
+
+        {currentPage > 1 && (
+          <button key={1} onClick={() => jumpToPage(1)}>
+            {1}
+          </button>
+        )}
+
+        {currentPage > 4 && (
+          <select
+            onChange={e => {
+              setOffset(+e.target.value * fetchLimit);
+              e.target.value = "...";
+            }}
+            defaultValue={"..."}
+          >
+            {renderPageNumberSelectOptions(2, currentPage - 3)}
+          </select>
+        )}
+
+        {renderPageNumberButtons()}
+
+        {currentPage < pages - 3 && (
+          <select
+            onChange={e => {
+              setOffset(+e.target.value * fetchLimit);
+              e.target.value = "...";
+            }}
+            defaultValue={"..."}
+          >
+            {renderPageNumberSelectOptions(currentPage + 3, pages - 1)}
+          </select>
+        )}
+
+        {currentPage < pages && (
+          <button key={pages} onClick={() => jumpToPage(pages)}>
+            {pages}
+          </button>
+        )}
+
+        <button onClick={getNext}>→</button>
+      </PageNavWrapper>
     </SearchSectionWrapper>
   );
 };
@@ -63,4 +160,32 @@ export default SearchSection;
 
 const SearchSectionWrapper = styled.section`
   grid-area: searchSection;
+`;
+
+const PageNavWrapper = styled.nav`
+  margin: 1em;
+  display: flex;
+  justify-content: center;
+
+  & > * {
+    cursor: pointer;
+    border: none;
+    background-color: transparent;
+    width: 2em;
+    &:first-child {
+      margin-right: 1em;
+    }
+    &:last-child {
+      margin-left: 1em;
+    }
+    &.active {
+      font-weight: bold;
+    }
+    &:not(.active) {
+      color: #696969;
+    }
+    &:not(.active):hover {
+      color: #000;
+    }
+  }
 `;
