11c37737bf460b085143144328b042b90910042a
ariavalenci <aria@valenci.no>
2023-09-26 14:02:48 +0200

    add icons and adjust styling, add parameters that adjust appearance of component

--- src/components/PokemonType.tsx
+++ src/components/PokemonType.tsx
@@ -3,14 +3,39 @@ import { typeColors } from "../data/typeColors";
 
 interface IPokemonType {
   typeName: string;
+  size?: number;
+  displayText?: boolean;
+  displayIcon?: boolean;
 }
 
-const PokemonType = ({ typeName }: IPokemonType) => {
+const PokemonType = ({
+  typeName,
+  size,
+  displayText,
+  displayIcon,
+}: IPokemonType) => {
   const typeColor = typeColors[typeName as keyof typeof typeColors];
 
   return (
-    <PokemonTypeWrapper $typeColor={typeColor}>
-      <span>{typeName}</span>
+    <PokemonTypeWrapper
+      $typeColor={typeColor}
+      className={`${size ? `type-size-${size}` : ""} ${
+        displayIcon !== false && displayText === false ? "icon-only" : ""
+      }`}
+    >
+      {displayIcon !== false && (
+        <img
+          style={{
+            mask: `url(./images/icons/type-icons/${typeName}.svg) no-repeat center`,
+            WebkitMask: `url(./images/icons/type-icons/${typeName}.svg) no-repeat center`,
+            maskSize: "cover",
+            WebkitMaskSize: "cover",
+          }}
+          alt=""
+        />
+      )}
+
+      {displayText !== false && <span>{typeName}</span>}
     </PokemonTypeWrapper>
   );
 };
@@ -22,19 +47,56 @@ interface IPokemonTypeWrapper {
 }
 
 const PokemonTypeWrapper = styled.span<IPokemonTypeWrapper>`
-  background-color: ${({ $typeColor }) => $typeColor}33;
-  margin: 0.3em;
-  padding: 0.5em;
+  background-color: color-mix(
+    in srgb,
+    ${({ $typeColor }) => $typeColor},
+    #fff 80%
+  );
+
   text-transform: uppercase;
-  font-size: 0.6em;
-  border-radius: 0.3em;
+  border-radius: 2em;
   color: ${({ $typeColor }) => $typeColor};
   align-items: center;
-  justify-content: center;
   display: inline-flex;
+  margin: 0.3em;
+  padding: 0.5em 0.8em;
+  font-size: 0.6em;
+  &.icon-only {
+    padding: 0.5em;
+  }
+  &.type-size {
+    &-1,
+    &-2,
+    &-3 {
+      margin: 0.1em;
+    }
+    &-1 {
+      font-size: 0.8em;
+    }
+    &-2 {
+      font-size: 0.9em;
+    }
+    &-3 {
+      font-size: 1.2em;
+    }
+  }
 
   & > span {
     color: ${({ $typeColor }) => $typeColor};
-    filter: invert(0.2) brightness(0.5);
+    font-family: "Barlow-SemiBold";
+    color: color-mix(in srgb, ${({ $typeColor }) => $typeColor}, #000000 30%);
+    margin-left: 0.3em;
+    margin-inline: 0.4em;
+  }
+
+  & > img {
+    padding: 0.4em;
+    width: 0px;
+    height: 0px;
+    background-color: color-mix(
+      in srgb,
+      ${({ $typeColor }) => $typeColor},
+      #00000050 50%
+    );
   }
 `;
